41cc08502eb7eb7624e3a67016f0a972a3834cf1
Zay Maung Maung Myint <zaymaungmaungmyint.info@gmail.com>
2025-05-19 01:06:13 +0700

    Add the data-type code

---
+++ 01-javascript-fundamentals/data-types.js
@@ -0,0 +1,21 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Ethan');
+console.log(typeof CODE); // undefined
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); // auto undefined because of not create value
+console.log(typeof year);
+
+year  = 1993;
+console.log(typeof year);
+
+console.log(typeof null); // take care using typeof because they have weird behavior
