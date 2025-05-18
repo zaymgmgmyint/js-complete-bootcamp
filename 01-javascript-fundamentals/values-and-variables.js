1889f63a2f2f36e8d61f2317744617b0029f68b7
Zay Maung Maung Myint <zaymaungmaungmyint.info@gmail.com>
2025-05-19 01:06:13 +0700

    Add the data-type code

---
+++ 01-javascript-fundamentals/values-and-variables.js
@@ -0,0 +1,12 @@
+let js = "amazing";
+console.log(js);
+
+console.log(40 + 8 + + 23 - 10);
+
+console.log("Ethan");
+console.log(23);
+
+let textValue = "Amazing";
+textValue = 40;
+console.log(textValue);
+
